b0a3ceda22a0d7549d35e9fdc357ad2337ecd96b
Donquixote <826714738@qq.com>
2023-07-13 16:17:03 +0800

    Atomic原子类

--- src/main/java/atomic/LongAccumulatorDemo.java
+++ src/main/java/atomic/LongAccumulatorDemo.java
@@ -5,6 +5,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
+/**
+ * 并行计算，不在乎执行顺序
+ */
 public class LongAccumulatorDemo {
     public static void main(String[] args) {
         LongAccumulator longAccumulator = new LongAccumulator((x, y) -> x + y, 0);
