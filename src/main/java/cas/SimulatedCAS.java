bad7a6eb63317bc0cd801e4d7862d24676f58a05
Donquixote <826714738@qq.com>
2023-07-13 17:09:41 +0800

    CAS

---
+++ src/main/java/cas/SimulatedCAS.java
@@ -0,0 +1,19 @@
+package cas;
+
+public class SimulatedCAS {
+
+    private volatile int value;
+
+    public synchronized int get() {
+        return value;
+    }
+
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
+        int oldValue = value;
+        if (oldValue == expectedValue) {
+            value = newValue;
+        }
+        return oldValue;
+    }
+    
+}
